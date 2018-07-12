f74c1aa73fd62c45efa4dba40f126f68e5768de5
Mindkid <prm.4555@gmail.com>
2018-07-12 01:28:59 +0100

    new bopt

--- source/main.c
+++ source/main.c
@@ -1,101 +1,167 @@
 #include <unistd.h>
-#include <stdint.h>
 #include <stdbool.h>
 #include <signal.h>
-#include <stdio.h>
 #include <malloc.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <pthread.h>
 #include <sys/mman.h>
 #include <sys/types.h>
-#include <libpmem.h>
-#include <math.h>
-#include "art.h"
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "list.h"
+
+//#include <libpmem.h>
+#include <semaphore.h>
 
 #define handle_error(msg) \
            do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
-#define MAP_SIZE 512000
+#define FLUSH(POINTER) asm("clflush (%0)" :: "r"(POINTER));
+
+#define NUMBER_OF_ELEMENTS 257
+
+#define MAP_SIZE 512
+#define MAP_FILE_NAME "../ramdisk/mapFile.dat"
+#define MAP_ADDR 0x7f49dcfc0000
+
+#define NUMBER_OF_OFFSET 3
+#define OFFSET_FILE_NAME "../ramdisk/offset.dat"
 
 
+/*
+*	This is the buffer were
+*	data are placed, the 
+*	savepointer and the 
+*	working pointer 
+*/
+Element* buffer = NULL;
+Element *savePointer = NULL;
+Element *workingPointer = NULL;
 
-// This is the budder were data are placed
 
-art_tree* buffer = NULL;
-// This pointer marks the start of a working page
-art_tree *savePointer = NULL;
+/*
+*	This is the map where
+*	it's stored the offset
+*/
+int* savePointerOffset = NULL;
+int* workingPointerOffset = NULL;
 
-// This pointer marks the working zone
-art_tree *workingPointer = NULL;
-
+/*
+*	This represent the size
+*	of the page and the size
+*	of the word bits
+*/
 long pageSize = 0;
-long wordBits = 0;
+long wordBytes = 0;
 
+/*
+*	This is the working thread
+*	this thread do the batchs 
+*	and waits for the semaphore
+*/
 pthread_t workingThread;
-bool working = false;
+pthread_t monitorThread;
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;;
 
-void* batchingTheFlushs();
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+/*	
+*	This are the semaphores for the
+*	monitoring and te batching threads
+*/
+sem_t workingSemaphore;
+
+int fileDescriptor;
+int offsetDescriptor;
+
+/*
+*	@TODO
+*	This are the functions heads
+*	move to header file when done	
+*/
+void* workingBatchThread();
+void* monitoringThread();
+void batchingTheFlushs();
 void handler(int sig, siginfo_t *si, void *unused);
 void setSignalHandler();
 void init();
 void recover();
+void correctOffsets();
+int openFile();
+void printStructure();
+void addElement(Element** head, Element* toAdd);
 
 
-
-void* batchingTheFlushs()
+void batchingTheFlushs()
 {
-    pthread_mutex_lock(&lock);
     
-    art_tree* nextPage = savePointer + pageSize;
-    art_tree* flushPointer = savePointer;
-    savePointer = nextPage;
+    Element* nextPage = savePointer + (pageSize - 1);
+    Element* flushPointer = savePointer;
+   
+    nextPage = (nextPage > workingPointer) ? workingPointer : nextPage;
     
-    printf("---- START FLUSHING ----\n"); 
-    while(flushPointer < nextPage)
+    while(flushPointer <= nextPage)
     {
-        asm("clflush (%0)" :: "r"(flushPointer));
-        
-        /*
-   		* This function it's to be used 
-   		* when the pmem lib works	
-   		* 
-   		*pmem_flush(flushPointer, wordBits);
-   		*/
-   		
-        flushPointer += wordBits;
+    	FLUSH(flushPointer);
+        //pmem_flush(flushPointer, wordBytes);
+        flushPointer += wordBytes;
     }
-    printf("---- DONE FLUSHING ----\n");
-    
-    pthread_mutex_unlock(&lock);
+    savePointer = nextPage;
+    *savePointerOffset = savePointer - buffer;
+    FLUSH(savePointerOffset);
+    FLUSH(workingPointerOffset);
 }
+
+void* monitoringThread()
+{
+	while(true)
+	{
+		if(workingPointer >= savePointer + pageSize)
+		{
+			sem_post(&workingSemaphore); 
+			pthread_mutex_lock(&mutex);
+		}
+	}
+}
+
+void* workingBatchThread()
+{
+	while(true)
+	{
+		sem_wait(&workingSemaphore);
+		batchingTheFlushs();
+		pthread_mutex_unlock(&mutex);
+	}
+}
+
 
 void handler(int sig, siginfo_t *si, void *unused)
 {
-	/*
-	* Treat the bad pointer here.
-	* This handler it's called when
-	* the program tries to access a
-	* tainted page.
-	*/
-	exit(EXIT_FAILURE);
+	switch(sig)
+	{
+ 		case SIGBUS:	
+ 			break;
+		case SIGSEGV:
+			break; 	
+	}
 }
 
 /*
 * This is the fucntion that sets
 * the signal handler
 */
+
 void setSignalHandler()
 {
-   struct sigaction sa;
-   sa.sa_flags = SA_SIGINFO;
-   sigemptyset(&sa.sa_mask);
-   sa.sa_sigaction = handler;
-   if (sigaction(SIGSEGV, &sa, NULL) == -1)
-       handle_error("sigaction");
+	struct sigaction sa;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_sigaction = handler;
+	if (sigaction(SIGSEGV, &sa, NULL) == -1)
+		handle_error("sigaction");
+	if (sigaction(SIGBUS, &sa, NULL) == -1)
+		handle_error("sigaction");
 }
+
 /*
 * This function it's the init
 * function, it happens when the
@@ -103,26 +169,60 @@ void setSignalHandler()
 */
 void init()
 {
-   /* 
-   * Allocate a buffer aligned on a page boundary;
-   * initial protection is PROT_READ | PROT_WRITE
-   * Map its anonymous, if the pmem does not work
-   * use a file to save the mapped region. 
-   */
+	bool fileCreated = true;
+	bool offsetFileCreated = true;
 	
-	buffer = (art_tree*) mmap(NULL, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_ANONYMOUS, 0, 0);
-   /*
-   * This function it's to be used 
-   * when the pmem lib works	
-   * 
-   *buffer = (art_tree*) pmem_map_file(NULL, MAP_SIZE, PMEM_FILE_CREATE|PMEM_FILE_TMPFILE, 0, NULL, NULL);
-   */
-   
-   savePointer = buffer;
-   workingPointer = savePointer;
-   
-   if (savePointer == NULL)
-       handle_error("mmap");
+	fileDescriptor = openFile(&fileCreated, MAP_FILE_NAME ,(MAP_SIZE * pageSize) - 1);
+	
+	offsetDescriptor = openFile(&offsetFileCreated, OFFSET_FILE_NAME, (NUMBER_OF_OFFSET * sizeof(int)) - 1);
+	
+	/* 
+	* Allocate a buffer aligned on a page boundary;
+	* initial protection is PROT_READ | PROT_WRITE 
+	*/
+	buffer = (Element*) mmap((void*)MAP_ADDR, (MAP_SIZE * pageSize) - 1, PROT_READ | PROT_WRITE, MAP_SHARED, fileDescriptor, 0);
+
+	savePointer = buffer;
+	workingPointer = buffer;
+	
+	int* offsets = (int*) mmap(0, (NUMBER_OF_OFFSET * sizeof(int)) - 1, PROT_READ | PROT_WRITE, MAP_SHARED, offsetDescriptor, 0);
+
+
+	savePointerOffset = offsets;
+	workingPointerOffset = offsets + sizeof(int);
+	//buffer = (Element*) pmem_map_file(NULL, MAP_SIZE, PMEM_FILE_TMPFILE, O_TMPFILE, NULL, NULL);
+
+	if(!fileCreated && !offsetFileCreated)
+	{
+		correctOffsets();
+	}
+		
+	if (savePointer == NULL)
+		handle_error("mmap");
+    
+    sem_init(&workingSemaphore, 0,0);
+}
+
+void printStructure()
+{
+	puts("It entered");
+	Element* toPrint = buffer;
+	while(toPrint->next != NULL)
+	{
+		printf("this is the value %d\n", toPrint->value);
+		printf("this is the pointer %p\n", toPrint->next);
+		toPrint = toPrint->next;
+	}
+}
+
+void correctOffsets()
+{
+	int offsetSP = *(savePointerOffset);	
+	if(offsetSP != 0)
+		savePointer += offsetSP;
+	int offsetWP = *(workingPointerOffset);
+	if(offsetWP != 0)
+		workingPointer += offsetWP;  
 }
 
 /*
@@ -131,45 +231,82 @@ void init()
 */
 void recover()
 {
-    if(mprotect(savePointer, pageSize, PROT_READ) == -1)
-        handle_error("mprotect");
-    
-    buffer += pageSize;
-    savePointer = buffer;
-    workingPointer = savePointer;
+    if(mprotect((void *)savePointer, pageSize, PROT_READ) == -1)
+    {
+        //handle_error("mprotect");
+        fprintf(stderr, "Value of errno: %d\n", errno);
+    }
+    savePointer += pageSize;
+	workingPointer = savePointer;
+    if(buffer + pageSize >= savePointer)
+    	buffer = savePointer;
 }
 
-void insertToTree(art_tree *t, const unsigned char *key, int key_len, void *value)
+/*
+*	@TODO
+*	This function masks the 
+*	adding of a element into 
+*	the list, it's necessary
+*	for abstraction purpose
+*/
+void addElement(Element** head, Element* toAdd)
 {
-	art_insert(buffer, key, key_len, value, (void**) &workingPointer);    
-   
-    /*if( (fmod(workingPointer, pageSize) == 0)
-    {
-        if(pthread_create(&workingThread, NULL, batchingTheFlushs, NULL) != 0)
-            handle_error("pthreadCreate");
-                 
-    }*/
+	addElementInList(head, toAdd);
+	*workingPointerOffset = workingPointer - buffer;
+}
+
+int openFile(bool* created, char* fileName, long size)
+{
+	int fd;
+	if(access(fileName, F_OK) != -1)
+	{ 
+    	fd = open(fileName, O_RDWR );
+    	*created = false;
+	}
+	else
+	{
+	 	fd = open(fileName, O_RDWR | O_CREAT , S_IRWXU);
+		lseek(fd, size, SEEK_SET);
+		write(fd, "", 1);
+	}
+	if(fd == -1)
+		handle_error("fopen");
+
+	return fd;
 }
 
 int main(int argc, char *argv[])
 {
-    pageSize = sysconf(_SC_PAGE_SIZE);
-    wordBits = sysconf(_SC_WORD_BIT); 
+    void* result;
+    int i;
     
-    if(pageSize == -1 || wordBits == -1)
+    pageSize = sysconf(_SC_PAGE_SIZE);
+    wordBytes = sysconf(_SC_WORD_BIT);  
+    
+    srand(time(NULL));
+    
+    if(pageSize == -1 || wordBytes == -1)
        handle_error("sysconf"); 
     
-    setSignalHandler();
+    //setSignalHandler();
     
-    if(savePointer == NULL)
-        init();    
+    init();
+        
     if(savePointer != workingPointer)
         recover();
+    			
+    if(pthread_create(&workingThread, NULL, workingBatchThread, NULL) != 0)
+		handle_error("pthreadCreate"); 
     
-    const unsigned char *key = "ola";
-    int key_len = 3;
-    int* value = (int*) 52012;
-    art_tree_ini(buffer);
-    insertToTree(buffer, key , key_len , value);
-          
+    if(pthread_create(&monitorThread, NULL, monitoringThread, NULL) != 0)
+		handle_error("pthreadCreate"); 
+    
+    
+    for(i = 0; i < NUMBER_OF_ELEMENTS ; i++)
+    	addElement(&buffer, generateElement(&workingPointer));     
+ 	
+ 	//printStructure();
+ 	
+ 	close(fileDescriptor);
+ 	close(offsetDescriptor);  
 }
