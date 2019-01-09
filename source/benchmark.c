d7994e6eec5d2500670d4bdc874370fe0f8ea4ea
mrmind <mrmind@debian.lan>
2019-01-09 19:20:28 +0000

    remove unnecessary comments

--- source/benchmark.c
+++ source/benchmark.c
@@ -187,66 +187,6 @@ void creatAndFillTestFile(char* fileName, int numberOfPages, int grain, int numb
   fprintf(file, "%s%s\n", CLOSE_OPERATION, FILE_DELIMITER);
 }
 
-/*
-int main(int argc, char *argv[])
-{
-  long keys[NUMBER_OF_ITERATIONS];
-
-  long fatherKey;
-  long key;
-  int value;
-
-  int i = 0;
-  int grain = 1;
-  int sizeOfEnum = inplaceInsertElement;
-
-  srand(RANDOM_SEED);
-
-  while(i < NUMBER_OF_ITERATIONS)
-  {
-    // Number between  1 - 100
-    int prob = ((rand() % MAX_PROBABILITY) + 1) %  sizeOfEnum;
-
-    switch (prob) {
-      case insertElement:
-          key = rand();
-          value = rand();
-          bopl_insert(key, sizeof(int), &value);
-          keys[keysOnBuffer] = key;
-          keysOnBuffer ++;
-          numberOfInserts ++;
-          break;
-      case updateElement:
-          key = keys[rand() % keysOnBuffer];
-          value = rand();
-          bopl_update(key, sizeof(int), &value);
-          numberOfUpdates++;
-          break;
-      case removeElement:
-          key = keys[rand() % keysOnBuffer];
-          bopl_remove(key);
-          numberOfRemoves++;
-          break;
-      case lookupElement:
-          key = keys[rand() % keysOnBuffer];
-          bopl_lookup(key);
-          numberOfSearchs++;
-          break;
-      case inplaceInsertElement:
-          key = rand();
-          value = rand();
-          fatherKey = keys[rand() % keysOnBuffer];
-          bopl_inplace_insert(fatherKey, key,sizeof(int), &value);
-          keys[keysOnBuffer] = key;
-          keysOnBuffer ++;
-          numberOfInplaceInsert++;
-          break;
-    }
-    i++;
-  }
-  return 0;
-}*/
-
 void help()
 {
   puts("");
