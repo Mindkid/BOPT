c47d2913f0ff9e203b732a900e567ba68620d910
Mindkid <prm.4555@gmail.com>
2019-03-05 16:22:54 +0000

    tested

--- benchmark/library.h
+++ benchmark/library.h
@@ -33,13 +33,13 @@
 #define ADD_OFFSET_TO_POINTER(prt, offset)  (int*) (((char*) prt) + offset)
 
 //CLFLUSH
-// #define FLUSH(POINTER) asm("clflush (%0)" :: "r"(POINTER));
+ #define FLUSH(POINTER) asm("clflush (%0)" :: "r"(POINTER));
 
 //CLFLUSHOPT
 // #define FLUSH(POINTER) asm("clflushopt (%0)" :: "r"(POINTER));
 
 //CLWB
- #define FLUSH(POINTER) asm("clwb (%0)" :: "r"(POINTER));
+// #define FLUSH(POINTER) asm("clwb (%0)" :: "r"(POINTER));
 
 #define FENCE asm("sfence");
 int openFile(char* fileName, unsigned long fileSize);
