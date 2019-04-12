7179ec7347b6234e6df09b7b24d02de7a873e6b9
mrmind <mrmind@debian.lan>
2019-04-12 02:41:17 +0100

    changed run.sh

--- source/run.sh
+++ source/run.sh
@@ -1,16 +1,61 @@
 #!/bin/bash
-
 TESTDIR=./tests/*.bopl
+TESTOPTANE=./testsOptane/*.bopl
 
-make
+help()
+{
+  echo "Use the commands:"
+  echo "    -a for all tests"
+  echo "    -o for optane"
+  echo "    -r for ram"
+  exit 1;
+}
 
-for f in $TESTDIR
-do
-  echo "Executing: $f"
-  rm -f ../ramdisk/*
-  ./boplFileReader.o -r $f
-  rm -f ../ramdisk/*
-  ./boplFileReaderSTTRAM.o -r $f
+runRam()
+{
+  echo "RAM - TESTING"
+  for f in $TESTDIR
+  do
+    echo "Executing: $f"
+    rm -f ../ramdisk/*
+    ./boplFileReader.o -r $f
+    rm -f ../ramdisk/*
+    ./boplFileReaderSTTRAM.o -r $f
+  done
+}
+
+runOptane()
+{
+  echo "OPTANE - TESTING"
+  for f in $TESTOPTANE
+  do
+    echo "Executing: $f"
+    rm -f ../ramdisk/*
+    ./boplFileReaderOPTANE.o -r $f
+  done
+}
+
+
+
+while getopts ":aor" o; do
+    case "${o}" in
+        a)
+            make
+            runRam
+            runOptane
+            ;;
+        o)
+            make
+            runOptane
+            ;;
+        r)
+            make
+            runRam
+            ;;
+        *)
+            help
+            ;;
+    esac
 done
 
 rm -f ../ramdisk/*
